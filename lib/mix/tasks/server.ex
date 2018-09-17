80673d3287aad1a72227ab4f7ecd0c102872a227
Marcos Ferreira <marcosp4f@gmail.com>
2018-09-16 23:59:26 -0300

    Add of a `server` task that starts a simple http server on the current dir
    It starts an :inets#httpd server on the current dir than sleeps for the
    infinity.
    Since if we don't sleep the process gets shutdown.

---
+++ lib/mix/tasks/server.ex
@@ -0,0 +1,52 @@
+defmodule Mix.Tasks.Server do
+  use Mix.Task
+
+  @shortdoc "Starts a basic HTTP server on the current dir"
+  @moduledoc """
+  It starts a basic HTTP server on the current directory
+
+  You can pass the following arguments:
+
+  - `--host`
+    Is the address that the server will bind to, can be a
+    FQDN or an IP address
+
+    Examples:
+      - `--host 0.0.0.0`
+      - `--host example.com`
+
+  - `--port`
+    The port that the server will bind to
+  """
+
+  @default_port 4000
+  @default_host 'localhost'
+  @shell Mix.shell()
+
+  def run(argv) do
+    {opts, _} = OptionParser.parse_head!(
+      argv,
+      switches: [
+        port: [:integer, :keep],
+        host: :string,
+      ]
+    )
+
+    host = if opts[:host], do: String.to_charlist(opts[:host]), else: @default_host
+    port = opts[:port] || @default_port
+
+    @shell.info("Running basic http server on #{host}:#{port}")
+
+    :inets.start()
+    :inets.start(:httpd, [
+      server_name: 'localhost',
+      port: port,
+      bind_address: host,
+      document_root: '.',
+      server_root: '.',
+      directory_index: ['index.html']
+    ])
+
+    Process.sleep(:infinity)
+  end
+end
